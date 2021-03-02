9919d112b7337d170eb33fb28bcde508c5b2015d
PC <PC@DESKTOP-CM088PI>
2021-03-01 19:27:50 -0500

    se agrego producto controller

--- src/java/pyp/controlador/ProductoController.java
+++ src/java/pyp/controlador/ProductoController.java
@@ -44,7 +44,6 @@ public class ProductoController implements Serializable{
          nuevoInsumoProducto.setInsumo(insumoSeleccionado);
          nuevoInsumoProducto.setCantidadInsumo(cantidad);
          insumoDelProducto.add(insumoProducto);
-         
     }
 
     public Producto getProducto() {
