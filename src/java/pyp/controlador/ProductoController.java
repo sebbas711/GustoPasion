eebbfc7b8d5c4cc5fdbb8fc61ae3f964b68586f1
PC <PC@DESKTOP-CM088PI>
2021-03-13 18:04:23 -0500

    Se dejo hecha la Carga Masiva de datos para el modulo de productos.

--- src/java/pyp/controlador/ProductoController.java
+++ src/java/pyp/controlador/ProductoController.java
@@ -7,11 +7,14 @@ package pyp.controlador;
 
 import com.mysql.jdbc.Connection;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
@@ -25,7 +28,15 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.FileUploadEvent;
+import pyp.DAO.ICategoriaProductoDAO;
 import pyp.DAO.IProductoDAO;
+import pyp.modelo.entidades.CategoriaProducto;
 import pyp.modelo.entidades.Insumo;
 import pyp.modelo.entidades.InsumosDelProducto;
 import pyp.modelo.entidades.Producto;
@@ -36,35 +47,37 @@ import pyp.modelo.entidades.Producto;
  */
 @Named
 @SessionScoped
-public class ProductoController implements Serializable{
-    
+public class ProductoController implements Serializable {
+
     @EJB
     private IProductoDAO productoDAO;
     private List<Producto> productos;
     private Producto productoSeleccionado;
-    
-    
+
     private Producto producto;
     private List<InsumosDelProducto> insumoDelProducto;
     private Insumo insumoSeleccionado;
     private Integer cantidad;
 
+    @EJB
+    ICategoriaProductoDAO ICategoriaDAO;
+
     public ProductoController() {
     }
-    
+
     @PostConstruct
-    public void init(){
+    public void init() {
         insumoDelProducto = new ArrayList<>();
-        
+
         producto = new Producto();
         producto.setInsumosDelProducto(insumoDelProducto);
     }
-    
-    public void agregarInsumo(InsumosDelProducto insumoProducto){
-         InsumosDelProducto nuevoInsumoProducto = new InsumosDelProducto();
-         nuevoInsumoProducto.setInsumo(insumoSeleccionado);
-         nuevoInsumoProducto.setCantidadInsumo(cantidad);
-         insumoDelProducto.add(insumoProducto);
+
+    public void agregarInsumo(InsumosDelProducto insumoProducto) {
+        InsumosDelProducto nuevoInsumoProducto = new InsumosDelProducto();
+        nuevoInsumoProducto.setInsumo(insumoSeleccionado);
+        nuevoInsumoProducto.setCantidadInsumo(cantidad);
+        insumoDelProducto.add(insumoProducto);
     }
 
     public Producto getProducto() {
@@ -82,12 +95,13 @@ public class ProductoController implements Serializable{
     public void setInsumoSeleccionado(Insumo insumoSeleccionado) {
         this.insumoSeleccionado = insumoSeleccionado;
     }
-    
-    public void seleccionarProducto(Producto p){
+
+    public void seleccionarProducto(Producto p) {
         System.out.println("Se ha seleccionado el usuario");
         System.out.println("p");
         this.productoSeleccionado = p;
     }
+
     public void descargaListado() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         ExternalContext context = facesContext.getExternalContext();
@@ -117,8 +131,8 @@ public class ProductoController implements Serializable{
             System.out.println("pyp.modelo.reportes.insumos.ListaInsumos()" + e.getMessage());
         }
     }
-    
-        public void descargaCantidadProducto() {
+
+    public void descargaCantidadProducto() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         ExternalContext context = facesContext.getExternalContext();
         HttpServletRequest request = (HttpServletRequest) context.getRequest();
@@ -147,6 +161,76 @@ public class ProductoController implements Serializable{
             System.out.println("pyp.modelo.reportes.insumos.ListaInsumos()" + e.getMessage());
         }
     }
-    
-    
+
+    public void insertarXLS(List cellDataList) {
+        try {
+            int filasContador = 0;
+            for (int i = 0; i < cellDataList.size(); i++) {
+                List cellTemp = (List) cellDataList.get(i);
+                Producto newP = new Producto();
+                for (int j = 0; j < cellTemp.size(); j++) {
+                    XSSFCell hssfCell = (XSSFCell) cellTemp.get(j);
+                    switch (j) {
+                        case 0:
+                            CategoriaProducto nueva = ICategoriaDAO.find((int) Math.floor(hssfCell.getNumericCellValue()));
+                            newP.setCategoriaProducto(nueva);
+                            filasContador++;
+                            break;
+                        case 1:
+                            newP.setNombre(hssfCell.toString());
+                            filasContador++;
+                            break;
+                        case 2:
+                            newP.setPrecio(hssfCell.getNumericCellValue());
+                            filasContador++;
+                            break;
+                        case 3:
+                            newP.setDescripcion(hssfCell.toString());
+                            filasContador++;
+                            break;
+                        case 4:
+                            newP.setImagenP(hssfCell.toString());
+                            filasContador++;
+                            break;
+                        case 5:
+                            newP.setEstado((int) Math.floor(hssfCell.getNumericCellValue()));
+                            filasContador++;
+                            break;
+                    }
+                }
+                productoDAO.create(newP);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cargaListaProductos(FileUploadEvent event) throws IOException {
+        InputStream input = event.getFile().getInputStream();
+        List cellData = new ArrayList();
+        try {
+            XSSFWorkbook workBook = new XSSFWorkbook(input);
+            XSSFSheet hssfSheet = workBook.getSheetAt(0);
+            Iterator rowIterator = hssfSheet.rowIterator();
+            rowIterator.next();
+
+            while (rowIterator.hasNext()) {
+                XSSFRow hssfRow = (XSSFRow) rowIterator.next();
+                Iterator iterator = hssfRow.cellIterator();
+                List cellTemp = new ArrayList();
+                while (iterator.hasNext()) {
+                    XSSFCell hssfCell = (XSSFCell) iterator.next();
+                    cellTemp.add(hssfCell);
+                }
+                cellData.add(cellTemp);
+            }
+            insertarXLS(cellData);
+        } catch (Exception e) {
+            PrimeFaces.current().executeScript("swal('Problemas ingresando el archivo' , 'error');");
+        }
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        context.redirect("ControlProductos.xhtml");
+    }
+
 }
